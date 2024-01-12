a2a34efc0207a30c8844bcfdb7c18fcf0a1c5c03
Angélica Macias <114703394+angelicamacias@users.noreply.github.com>
2024-01-11 23:05:02 -0600

    Update app.py
    Remove the create_app function and instead create an app instance directly in the app.py module.
    Update the code to use this app instance.

--- app.py
+++ app.py
@@ -6,27 +6,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    client = MongoClient(os.getenv("MONGODB_URI"))
-    app.db = client.catblog
+client = MongoClient(os.getenv("MONGODB_URI"))
+app.db = client.catblog
 
-    @app.route("/", methods=["GET", "POST"])
-    def cat_blog():
-        #print([e for e in app.db.entries.find({})])
-        if request.method == "POST":
-            entry_content = request.form.get("content")
-            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
-            
-        entries_with_date = [
-            (
-                entry["content"],
-                entry["date"],
-                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
-            )
-            for entry in app.db.entries.find({})
-        ]
-        return render_template("home.html", entries=entries_with_date)
-    return app 
+@app.route("/", methods=["GET", "POST"])
+def cat_blog():
+    #print([e for e in app.db.entries.find({})])
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+        
+    entries_with_date = [
+        (
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in app.db.entries.find({})
+    ]
+    return render_template("home.html", entries=entries_with_date)
+
+if __name__ == "__main__":
+    app.run(debug=True)
